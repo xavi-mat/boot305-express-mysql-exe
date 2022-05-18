5ffacc906308559b91605e76ae47b3c7e7c570fb
Xavimat <xaviermatoses@gmail.com>
2022-05-18 18:57:01 +0200

    Products route and controller

---
+++ routes/products.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const ProductController = require("../controllers/ProductController");
+const router = express.Router();
+
+router.get('/', ProductController.listAll);
+router.get('/categories', ProductController.listWithCategories);
+router.get('/id/:id', ProductController.getById);
+router.get('/search/:name', ProductController.searchByName);
+router.get('/desc', ProductController.listDescending);
+router.post('/', ProductController.addPost);
+router.put('/:id', ProductController.updateById);
+router.delete('/:id', ProductController.delete);
+
+module.exports = router;
