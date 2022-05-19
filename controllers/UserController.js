1bd86cd39d8165385189e324b2e5a0a6b581106a
Xavimat <xaviermatoses@gmail.com>
2022-05-19 18:59:03 +0200

    endpoint: Show all users

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -19,6 +19,13 @@ const UserController = {
             if (err) throw err;
             res.send(result);
         })
+    },
+    listAll(req, res) {
+        let sql = 'SELECT * FROM user';
+        db.query(sql, (err, result) => {
+            if (err) throw err;
+            res.send(result);
+        })
     }
 };
 
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const UserController = require("../controllers/UserController");
 const router = express.Router();
 
+router.get('/', UserController.listAll);
 router.post('/', UserController.addUser);
 router.put('/:id', UserController.updateUser);
 
